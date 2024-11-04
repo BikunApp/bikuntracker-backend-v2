20c34f60e5391280f8c779812dd9e2abba721289
Adrian <adrian.hamzah15@gmail.com>
2024-11-04 18:42:30 +0700

    feat: implement missing method on hopehelps repo

--- app/interfaces/hopehelps.go
+++ app/interfaces/hopehelps.go
@@ -11,4 +11,5 @@ type ReportRepository interface {
 	GetReports(ctx context.Context) (reports []models.Report, err error)
 	GetReportById(ctx context.Context) (report *models.Report, err error)
 	CreateReport(ctx context.Context, data dto.CreateReportRequestBody) (report *models.Report, err error)
+	DeleteReport(ctx context.Context, id string) (err error)
 }
--- db/migrations/000005_create_report_table.down.sql
+++ db/migrations/000005_create_report_table.down.sql
@@ -1,2 +1,2 @@
-DROP TRIGGER IF EXIST update_report_updated_at;
-DROP TABLE IF EXIST report;
+DROP TRIGGER IF EXISTS update_report_updated_at on report;
+DROP TABLE IF EXISTS report;
